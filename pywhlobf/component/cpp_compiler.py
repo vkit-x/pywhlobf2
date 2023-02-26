dc589d79616286f88da1e959029582dd0e6cc82e
Hunt Zhan <huntzhan.dev@gmail.com>
2023-02-26 15:39:52 +0800

    feat: setup ci & add CppGenerator. (#1)
    * feat: add CppGenerator.
    
    * add gh actions.
    
    * fix iolite dep.
    
    * msg.
    
    * lint
    
    * adjust.
    
    * adjust.
    
    * adjust.
    
    * Prepare test data.
    
    * Prepare test data.
    
    * test_cpp_generator
    
    * test cython3.
    
    * test cython3.

---
+++ pywhlobf/component/cpp_compiler.py
@@ -0,0 +1,7 @@
+# from typing import Mapping, Union
+import attrs
+
+
+@attrs.define
+class CppCompilerConfig:
+    pass
---
+++ pywhlobf/component/cpp_generator.py
@@ -0,0 +1,51 @@
+from typing import Mapping, Union
+from pathlib import Path
+import shutil
+
+import attrs
+from Cython.Build.Dependencies import cythonize
+from distutils.extension import Extension
+
+
+@attrs.define
+class CppGeneratorConfig:
+    # See:
+    # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#Cython.Build.cythonize
+    # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#compiler-directives
+    compiler_directives: Mapping[str, Union[bool, str]] = attrs.field(factory=dict)
+    options: Mapping[str, Union[bool, int, str]] = attrs.field(factory=dict)
+
+
+class CppGenerator:
+
+    def __init__(self, config: CppGeneratorConfig):
+        self.config = config
+
+    def run(self, py_file: Path, working_fd: Path):
+        # Copy python file to the working folder.
+        assert working_fd.is_dir()
+        working_py_file = working_fd / py_file.name
+        shutil.copyfile(py_file, working_py_file)
+        py_file = working_py_file
+
+        # The `cythonize` function generate a c++ file alongside.
+        compiler_directives = dict(self.config.compiler_directives)
+        # NOTE: Make sure you know what you are doing if you pass the `language_level`.
+        if 'language_level' not in compiler_directives:
+            compiler_directives['language_level'] = '3'
+
+        ext_modules = cythonize(
+            module_list=[str(py_file)],
+            # NOTE: Passing language will trigger a warning message, that is ok.
+            language='c++',
+            compiler_directives=compiler_directives,
+            **self.config.options,
+        )
+        assert ext_modules is not None
+        assert len(ext_modules) == 1
+        ext_module: Extension = ext_modules[0]
+
+        cpp_file = py_file.with_suffix('.cpp')
+        assert cpp_file.is_file()
+
+        return cpp_file, ext_module
