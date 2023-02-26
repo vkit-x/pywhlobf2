df00a6597cfa3745c599e097375ba7a3462e2560
Hunt Zhan <huntzhan.dev@gmail.com>
2023-02-26 19:05:28 +0800

    add test; adjusted.

---
+++ tests/test_string_literal_obfuscator.py
@@ -0,0 +1,18 @@
+from pywhlobf.component.cpp_generator import CppGeneratorConfig, CppGenerator
+from pywhlobf.component.string_literal_obfuscator import (
+    StringLiteralObfuscatorConfig,
+    StringLiteralObfuscator,
+)
+from tests.opt import get_test_output_fd, get_test_py_file
+
+
+def test_test_string_literal_obfuscator():
+    output_fd = get_test_output_fd()
+    test_py_file = get_test_py_file()
+
+    cpp_generator = CppGenerator(CppGeneratorConfig())
+    cpp_file, _ = cpp_generator.run(test_py_file, output_fd)
+
+    string_literal_obfuscator = StringLiteralObfuscator(StringLiteralObfuscatorConfig())
+    include_fd = string_literal_obfuscator.run(cpp_file)
+    assert include_fd
