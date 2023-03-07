d2ee09eef347be0acefab0ffd116513907f25d7b
Hunt Zhan <huntzhan.dev@gmail.com>
2023-03-07 19:13:13 +0800

    patch PyInit___init__.

--- pywhlobf/component/cpp_generator.py
+++ pywhlobf/component/cpp_generator.py
@@ -1,6 +1,7 @@
 from typing import Mapping, Union
 from pathlib import Path
 import shutil
+import re
 
 import attrs
 from setuptools import Extension
@@ -67,5 +68,15 @@ class CppGenerator:
 
         # Make sure the cpp file is generated.
         assert cpp_file.is_file()
+
+        if py_file.stem == '__init__':
+            code = cpp_file.read_text()
+            shutil.copyfile(cpp_file, cpp_file.with_suffix('.cpp.bak_before_patching_init'))
+            code = re.sub(
+                rf'PyInit_{py_file.parent.name}\(void\)',
+                'PyInit___init__(void)',
+                code,
+            )
+            cpp_file.write_text(code)
 
         return cpp_file, ext_module
