898fa38b7488ed808e73c6d6bc00c1b6776cbff7
Hunt Zhan <huntzhan.dev@gmail.com>
2023-07-04 11:31:38 +0800

    fix: docker entrypoint.sh.

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -10,19 +10,19 @@ if [ -z "$PYTHON_VERSION" ]; then
 fi
 
 if [[ "$PYTHON_VERSION" == "3.8" ]]; then
-    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp38-cp38'
+    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp38'
 elif [[ "$PYTHON_VERSION" == "3.9" ]]; then
-    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp39-cp39'
+    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp39'
 elif [[ "$PYTHON_VERSION" == "3.10" ]]; then
-    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp310-cp310'
+    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp310'
 elif [[ "$PYTHON_VERSION" == "3.11" ]]; then
-    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp311-cp311'
+    export PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG='cp311'
 else
     echo "FATEL: Invalid PYTHON_VERSION={PYTHON_VERSION}. Abort."
     exit 1
 fi
 
-PYWHLOBF="/opt/python/${PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG}/bin/pywhlobf"
+PYWHLOBF="/opt/python/${PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG}-${PYWHLOBF_WHEEL_FILE_OUTPUT_ABI_TAG}/bin/pywhlobf"
 if [ ! -f "$PYWHLOBF" ]; then
     echo "FATEL: PYWHLOBF=${PYWHLOBF} not found. Abort."
     exit 1
